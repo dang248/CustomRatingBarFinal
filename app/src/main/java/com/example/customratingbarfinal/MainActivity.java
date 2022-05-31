d2134152fa6eefa7a097466a3ba5110a21b52fcc
dang248 <lequocdangbku18@gmail.com>
2022-05-31 16:57:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/customratingbarfinal/MainActivity.java
@@ -0,0 +1,37 @@
+
+
+package com.example.customratingbarfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txtValueRate;
+    RatingBar ratingBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addControls();
+        addEvents();
+    }
+    private void addEvents() {
+        txtValueRate.setText(ratingBar.getRating()+"/5");
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                txtValueRate.setText(ratingBar.getRating()+"/5");
+
+            }
+        });
+
+    }
+    private void addControls() {
+        txtValueRate = findViewById(R.id.textViewValueRate);
+        ratingBar = findViewById(R.id.ratingBar);
+
+    }
+}
